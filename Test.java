a3fc12d3420b2640f1ad65c84546099363d22660
anita0007 <114204165+anita0007@users.noreply.github.com>
2022-09-23 11:53:20 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,7 +1,10 @@
 public class Test{
     public static void main(String[] args){
         System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println("Hello");
+        printStr("Anita");
     }
+    
+        public static void printStr(String str) {
+        System.out.println("My Name = " + str);
+    } 
 }
