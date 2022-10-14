1dfeb5ff30fd81146f8492a321ffd5affb39a991
anita0007 <114204165+anita0007@users.noreply.github.com>
2022-10-14 11:49:47 +0800

    2022-10-14
    對稱塔

---
+++ Test10.java
@@ -0,0 +1,20 @@
+public class Test10{
+    public static void main(String[] args){
+        int row = 0;
+		int t;
+		
+        while (row < 9 ) {
+			t = row;
+			if (row >= 5 ) { 
+				t = 8 - row;
+			}
+			int star = 0;
+			while (star <= t){
+				System.out.print("*");
+				star = star + 1 ;
+			}					
+			System.out.println("");
+			row = row + 1;	
+        }
+    }
+}
