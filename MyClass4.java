1976348a40b3d8223319c1e2fcc81fde0ddb9717
anita0007 <114204165+anita0007@users.noreply.github.com>
2023-01-06 10:59:59 +0800

    2023-01-06

---
+++ MyClass4.java
@@ -0,0 +1,10 @@
+public class MyClass4 {
+	public static void main (String[] args) {	
+		int total = 0;
+		for (String str : args) {
+			total+= Integer.parseInt(str);
+			System.out.println("args[] "+str);
+		}
+		System.out.println("total" +total);
+	}
+}
